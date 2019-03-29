f0dea75d253c04dbf43edcc07b481a253b71c5d9
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-03-29 13:25:37 -0600

    arga inicial

---
+++ src/TenDaysOfXamarin/ViewModels/MainVM.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using TenDaysOfXamarin.Model;
+
+namespace TenDaysOfXamarin.ViewModels
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+                OnPropertyChanged("CanSave");
+            }
+        }
+
+
+        private string query;
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private Venue selectedVenue; 
+        public Venue SelectedVenue
+        {
+            get { return selectedVenue; }
+            set
+            {
+                selectedVenue = value;
+                if (selectedVenue != null)
+                {
+                    ShowVenues = false;
+                    Query = string.Empty;
+                }
+                OnPropertyChanged("SelectedVenue");
+                OnPropertyChanged("ShowSelectedVenue");
+            }
+        }
+
+        private string content;
+        public string Content
+        {
+            get { return content; }
+            set
+            {
+                content = value;
+                OnPropertyChanged("Content");
+                OnPropertyChanged("CanSave");
+
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool CanSave
+        {
+            get { return !string.IsNullOrWhiteSpace(Title) && !string.IsNullOrWhiteSpace(Content); }
+        }
+
+        public bool ShowSelectedVenue
+        {
+            get { return SelectedVenue != null; }
+        }
+
+        private bool showVenues;
+        public bool ShowVenues
+        {
+            get { return showVenues; }
+            set
+            {
+                showVenues = value;
+                OnPropertyChanged("ShowVenues");
+            }
+        }
+
+    }
+}
